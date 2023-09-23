44573cc1e512aa8875a03d57a32df927f78ef579
kartheekramanaboina <kartheekramanaboina08@gmail.com>
2023-09-23 13:46:44 +0530

    todo

---
+++ todo.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const taskInput = document.getElementById("task");
+    const addTaskButton = document.getElementById("addTask");
+    const taskList = document.getElementById("taskList");
+
+    addTaskButton.addEventListener("click", function () {
+        const taskText = taskInput.value.trim();
+
+        if (taskText !== "") {
+            const listItem = document.createElement("li");
+            listItem.innerHTML = `
+                <span>${taskText}</span>
+                <button class="delete">Delete</button>
+            `;
+
+            taskList.appendChild(listItem);
+            taskInput.value = "";
+            const deleteButton = listItem.querySelector(".delete");
+            deleteButton.addEventListener("click", function () {
+                listItem.remove();
+            });
+        }
+    });
+    taskInput.addEventListener("keyup", function (event) {
+        if (event.key === "Enter") {
+            addTaskButton.click();
+        }
+    });
+});
